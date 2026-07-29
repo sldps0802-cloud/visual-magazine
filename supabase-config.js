768b1c1d57da32bd0df1343294f03c474b994a5e
vctest <sldps0802@gmail.com>
2026-07-29 13:23:29 +0900

    Wire up Supabase backend so posts are shared across all visitors

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+var SUPABASE_URL = 'https://qvsptsspvbzjukjbdwdj.supabase.co';
+var SUPABASE_KEY = 'sb_publishable_Wn9BkVEmKZgSq0uicBVx5A_Zevwunor';
+var db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
